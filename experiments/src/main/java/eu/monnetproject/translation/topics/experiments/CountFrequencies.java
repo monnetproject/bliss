0d127c16dbae38b6585993c7cde35032151d5807
John McCrae <john@mccr.ae>
2012-10-08 13:46:01 +0200

    Add thresholding as well

--- experiments/src/main/java/eu/monnetproject/translation/topics/experiments/CountFrequencies.java
+++ experiments/src/main/java/eu/monnetproject/translation/topics/experiments/CountFrequencies.java
@@ -1,4 +1,5 @@
-/*********************************************************************************
+/**
+ * *******************************************************************************
  * Copyright (c) 2011, Monnet Project All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -31,6 +32,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 
 /**
  *
@@ -41,63 +44,102 @@ public class CountFrequencies {
     private static void fail(String message) {
         System.err.println(message);
         System.err.println("\nUsage:\n"
-                + "\tmvn exec:java -Dexec.mainClass=\""+CountFrequencies.class.getName()+"\" -Dexec.args=\"corpus W\"");
+                + "\tmvn exec:java -Dexec.mainClass=\"" + CountFrequencies.class.getName() + "\" -Dexec.args=\"corpus W topN out\"");
         System.exit(-1);
     }
-    
+
     public static void main(String[] args) throws Exception {
-        if(args.length != 3) {
+        if (args.length != 4) {
             fail("Wrong number of arguments");
         }
         final File corpusFile = new File(args[0]);
-        
-        if(!corpusFile.exists() || !corpusFile.canRead()) {
+
+        if (!corpusFile.exists() || !corpusFile.canRead()) {
             fail("Cannot access corpusFile");
         }
-        
+
         final int W;
         try {
             W = Integer.parseInt(args[1]);
-        } catch(NumberFormatException x) {
+        } catch (NumberFormatException x) {
             fail("Not an integer " + args[1]);
             return;
         }
-        
-        final File outFile = new File(args[2]);
-        
-        if(outFile.exists() && !outFile.canWrite()) {
+
+        final int topN;
+        try {
+            topN = Integer.parseInt(args[2]);
+        } catch (NumberFormatException x) {
+            fail("Not an integer " + args[2]);
+            return;
+        }
+
+        final File outFile = new File(args[3]);
+
+        if (outFile.exists() && !outFile.canWrite()) {
             fail("Cannot access outFile");
         }
-        
-        final DataInputStream dataIn = new DataInputStream(new FileInputStream(corpusFile));
+
+        final DataInputStream dataIn;
+
+        if (corpusFile.getName().endsWith(".gz")) {
+            dataIn = new DataInputStream(new GZIPInputStream(new FileInputStream(corpusFile)));
+        } else if (corpusFile.getName().endsWith(".bz2")) {
+            dataIn = new DataInputStream(new BZip2CompressorInputStream(new FileInputStream(corpusFile)));
+        } else {
+            dataIn = new DataInputStream(new FileInputStream(corpusFile));
+        }
         final int[] freqs = new int[W];
         final DataOutputStream dataOut = new DataOutputStream(new FileOutputStream(outFile));
         
-        calcFreqs(dataIn, freqs);
+        final int freqThresh = calcFreqs(dataIn, freqs, topN);
         
+        System.out.println("Top "+topN+" words frequency > " + freqThresh);
+
         printFreqs(freqs, dataOut);
     }
 
-    private static void calcFreqs(DataInputStream dataIn, int[] freqs) throws IOException {
+    private static int calcFreqs(DataInputStream dataIn, int[] freqs, int topN) throws IOException {
+        int[] topNvalues = new int[topN];
+        int[] topNkeys = new int[topN];
         int n = 0;
         try {
-            while(dataIn.available() > 0) {
-                int i = dataIn.readInt();
-                if(i != 0) {
-                    freqs[i]++;
-                }
-                if(++n % 100000 == 0) {
+            DATA_LOOP:
+            while (dataIn.available() > 0) {
+                if (++n % 100000 == 0) {
                     System.err.print(".");
+                }
+                int i = dataIn.readInt();
+                if (i != 0) {
+                    freqs[i]++;
+                    for (int j = 0; j < topN; j++) {
+                        if (topNkeys[j] == i) {
+                            topNvalues[j]++;
+                            continue DATA_LOOP;
+                        }
+                    }
+                    for (int j = 0; j < topN; j++) {
+                        if(freqs[i] > topNvalues[j]) {
+                            topNvalues[j] = freqs[i];
+                            topNkeys[j] = i;
+                            continue DATA_LOOP;
+                        }
+                    }
                 }
             }
         } finally {
             dataIn.close();
         }
+        int freqThresh = Integer.MAX_VALUE;
+        for(int j = 0; j < topN; j++) {
+            freqThresh = Math.min(freqThresh,topNvalues[j]);
+        }
+        return freqThresh;
     }
 
     private static void printFreqs(int[] freqs, DataOutputStream dataOut) throws IOException {
         try {
-            for(int i = 1; i < freqs.length; i++) {
+            for (int i = 1; i < freqs.length; i++) {
                 dataOut.writeInt(freqs[i]);
             }
         } finally {
@@ -105,3 +147,4 @@ public class CountFrequencies {
         }
     }
 }
+ 
