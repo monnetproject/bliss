082e9d13c4d5d728f9149a3767c9abd2cc677776
John McCrae <john@mccr.ae>
2013-04-03 10:04:29 +0100

    Better check for W mismatch

--- experiments/clesa-battery.sh
+++ experiments/clesa-battery.sh
@@ -2,144 +2,144 @@
 
 echo "Wikipedia en-es"
 echo "Normalized TF"
-./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test
+./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test
 echo "Normalized DF"
-./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test tfNorm=false
+./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test tfNorm=false
 echo "Normalized log(TF)"
-./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test clesaMethod=LOG_NORMALIZED
+./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test clesaMethod=LOG_NORMALIZED
 echo "Normalized log(DF)"
-./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test clesaMethod=LOG_NORMALIZED tfNorm=false
+./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test clesaMethod=LOG_NORMALIZED tfNorm=false
 echo "TFIDF"
-./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test clesaMethod=TFIDF
+./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test clesaMethod=TFIDF
 echo "LUCENE"
-./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test clesaMethod=LUCENE
+./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test clesaMethod=LUCENE
 echo "OKAPI_BM25"
-./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test clesaMethod=OKAPI_BM25
+./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test clesaMethod=OKAPI_BM25
 echo "SORG"
-./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test clesaMethod=SORG
+./mate-finding ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test clesaMethod=SORG
 
 echo "Wikipedia en-de"
 echo "Normalized TF"
-./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test
+./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test
 echo "Normalized DF"
-./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test tfNorm=false
+./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test tfNorm=false
 echo "Normalized log(TF)"
-./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test clesaMethod=LOG_NORMALIZED
+./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test clesaMethod=LOG_NORMALIZED
 echo "Normalized log(DF)"
-./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test clesaMethod=LOG_NORMALIZED tfNorm=false
+./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test clesaMethod=LOG_NORMALIZED tfNorm=false
 echo "TFIDF"
-./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test clesaMethod=TFIDF
+./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test clesaMethod=TFIDF
 echo "LUCENE"
-./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test clesaMethod=LUCENE
+./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test clesaMethod=LUCENE
 echo "OKAPI_BM25"
-./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test clesaMethod=OKAPI_BM25
+./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test clesaMethod=OKAPI_BM25
 echo "SORG"
-./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test clesaMethod=SORG
+./mate-finding ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test clesaMethod=SORG
 
 echo "Acquis en-es"
 echo "Normalized TF"
-./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test
+./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test
 echo "Normalized DF"
-./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test tfNorm=false
+./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test tfNorm=false
 echo "Normalized log(TF)"
-./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test clesaMethod=LOG_NORMALIZED
+./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test clesaMethod=LOG_NORMALIZED
 echo "Normalized log(DF)"
-./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test clesaMethod=LOG_NORMALIZED tfNorm=false
+./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test clesaMethod=LOG_NORMALIZED tfNorm=false
 echo "TFIDF"
-./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test clesaMethod=TFIDF
+./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test clesaMethod=TFIDF
 echo "LUCENE"
-./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test clesaMethod=LUCENE
+./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test clesaMethod=LUCENE
 echo "OKAPI_BM25"
-./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test clesaMethod=OKAPI_BM25
+./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test clesaMethod=OKAPI_BM25
 echo "SORG"
-./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test clesaMethod=SORG
+./mate-finding ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test clesaMethod=SORG
 
 echo "Acquis de-en"
 echo "Normalized TF"
-./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test
+./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test
 echo "Normalized DF"
-./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test tfNorm=false
+./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test tfNorm=false
 echo "Normalized log(TF)"
-./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test clesaMethod=LOG_NORMALIZED
+./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test clesaMethod=LOG_NORMALIZED
 echo "Normalized log(DF)"
-./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test clesaMethod=LOG_NORMALIZED tfNorm=false
+./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test clesaMethod=LOG_NORMALIZED tfNorm=false
 echo "TFIDF"
-./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test clesaMethod=TFIDF
+./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test clesaMethod=TFIDF
 echo "LUCENE"
-./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test clesaMethod=LUCENE
+./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test clesaMethod=LUCENE
 echo "OKAPI_BM25"
-./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test clesaMethod=OKAPI_BM25
+./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test clesaMethod=OKAPI_BM25
 echo "SORG"
-./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test clesaMethod=SORG
+./mate-finding ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test clesaMethod=SORG
 
 echo "Wikipedia es-en"
 echo "Normalized TF"
-./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test
+./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test
 echo "Normalized DF"
-./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test tfNorm=false
+./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test tfNorm=false
 echo "Normalized log(TF)"
-./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test clesaMethod=LOG_NORMALIZED
+./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test clesaMethod=LOG_NORMALIZED
 echo "Normalized log(DF)"
-./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test clesaMethod=LOG_NORMALIZED tfNorm=false
+./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test clesaMethod=LOG_NORMALIZED tfNorm=false
 echo "TFIDF"
-./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test clesaMethod=TFIDF
+./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test clesaMethod=TFIDF
 echo "LUCENE"
-./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test clesaMethod=LUCENE
+./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test clesaMethod=LUCENE
 echo "OKAPI_BM25"
-./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test clesaMethod=OKAPI_BM25
+./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test clesaMethod=OKAPI_BM25
 echo "SORG"
-./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es.test clesaMethod=SORG
+./mate-finding -inv ../corpora/wiki-en-es-train CLESA ../corpora/wiki-en-es.wordMap ../corpora/wiki-en-es-test clesaMethod=SORG
 
 echo "Wikipedia de-en"
 echo "Normalized TF"
-./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test
+./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test
 echo "Normalized DF"
-./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test tfNorm=false
+./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test tfNorm=false
 echo "Normalized log(TF)"
-./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test clesaMethod=LOG_NORMALIZED
+./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test clesaMethod=LOG_NORMALIZED
 echo "Normalized log(DF)"
-./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test clesaMethod=LOG_NORMALIZED tfNorm=false
+./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test clesaMethod=LOG_NORMALIZED tfNorm=false
 echo "TFIDF"
-./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test clesaMethod=TFIDF
+./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test clesaMethod=TFIDF
 echo "LUCENE"
-./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test clesaMethod=LUCENE
+./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test clesaMethod=LUCENE
 echo "OKAPI_BM25"
-./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test clesaMethod=OKAPI_BM25
+./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test clesaMethod=OKAPI_BM25
 echo "SORG"
-./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de.test clesaMethod=SORG
+./mate-finding -inv ../corpora/wiki-en-de-train CLESA ../corpora/wiki-en-de.wordMap ../corpora/wiki-en-de-test clesaMethod=SORG
 
 echo "Acquis es-en"
 echo "Normalized TF"
-./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test
+./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test
 echo "Normalized DF"
-./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test tfNorm=false
+./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test tfNorm=false
 echo "Normalized log(TF)"
-./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test clesaMethod=LOG_NORMALIZED
+./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test clesaMethod=LOG_NORMALIZED
 echo "Normalized log(DF)"
-./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test clesaMethod=LOG_NORMALIZED tfNorm=false
+./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test clesaMethod=LOG_NORMALIZED tfNorm=false
 echo "TFIDF"
-./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test clesaMethod=TFIDF
+./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test clesaMethod=TFIDF
 echo "LUCENE"
-./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test clesaMethod=LUCENE
+./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test clesaMethod=LUCENE
 echo "OKAPI_BM25"
-./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test clesaMethod=OKAPI_BM25
+./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test clesaMethod=OKAPI_BM25
 echo "SORG"
-./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es.test clesaMethod=SORG
+./mate-finding -inv ../corpora/acquis-en-es-train CLESA ../corpora/acquis-en-es.wordMap ../corpora/acquis-en-es-test clesaMethod=SORG
 
 echo "Acquis en-de"
 echo "Normalized TF"
-./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test
+./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test
 echo "Normalized DF"
-./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test tfNorm=false
+./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test tfNorm=false
 echo "Normalized log(TF)"
-./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test clesaMethod=LOG_NORMALIZED
+./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test clesaMethod=LOG_NORMALIZED
 echo "Normalized log(DF)"
-./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test clesaMethod=LOG_NORMALIZED tfNorm=false
+./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test clesaMethod=LOG_NORMALIZED tfNorm=false
 echo "TFIDF"
-./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test clesaMethod=TFIDF
+./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test clesaMethod=TFIDF
 echo "LUCENE"
-./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test clesaMethod=LUCENE
+./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test clesaMethod=LUCENE
 echo "OKAPI_BM25"
-./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test clesaMethod=OKAPI_BM25
+./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test clesaMethod=OKAPI_BM25
 echo "SORG"
-./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en.test clesaMethod=SORG
+./mate-finding -inv ../corpora/acquis-de-en-train CLESA ../corpora/acquis-de-en.wordMap ../corpora/acquis-de-en-test clesaMethod=SORG
